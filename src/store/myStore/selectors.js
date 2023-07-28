a820921b16ee3d51548779660ca6d0198786e3d0
bigcakeSg <sebastiengirard@yahoo.fr>
2023-07-28 14:47:49 +0200

    fix selector => createSelector

--- src/store/myStore/selectors.js
+++ src/store/myStore/selectors.js
@@ -2,9 +2,11 @@ import { createSelector } from 'reselect';
 
 const myStore = (state) => state.myStore;
 
-export const getMyStoreLoading = (state) => myStore(state).loading;
+export const getMyStoreLoading = createSelector(
+  myStore,
+  (store) => store.loading
+);
 
-// export const getMyStoreData = (state) => myStore(state).data;
 export const getMyStoreData = createSelector(myStore, (store) => store.data);
 
-export const getMyStoreError = (state) => myStore(state).error;
+export const getMyStoreError = createSelector(myStore, (store) => store.error);
