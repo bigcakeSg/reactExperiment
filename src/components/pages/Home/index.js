e9049ef3946a931bb756f4fa12513f02510c9a5d
Sébastien Girard <95628952+bigcakeSg@users.noreply.github.com>
2023-08-09 12:17:24 +0200

    create router config (#6)

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -2,12 +2,6 @@ const Home = (userId, setUserId) => {
   return (
     <>
       <h1>Home Page</h1>
-      <input type="text" />
-      {/* <input
-        type="text"
-        value={userId}
-        onChange={(e) => setUserId(e.target.value)}
-      /> */}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,47 +7,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-import rootReducer from './store';
 
 import './styles/global-styles.css';
-import App from './App';
-import Home from './components/pages/Home';
-import FirstPage from './components/pages/FirstPage';
-import { getStoreData } from './store/myStore/thunks';
+
+import rootReducer from './store';
+import Root from './routerConfig';
 
 const store = configureStore({ reducer: rootReducer, devTools: true });
-
-const Root = () => {
-  const dispatch = useDispatch();
-
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <>OUPS !</>,
-      children: [
-        {
-          path: '',
-          element: <Home />
-        },
-        {
-          path: 'first-page/:userId',
-          element: <FirstPage />,
-          loader: ({ params }) => {
-            dispatch(getStoreData(params.userId));
-            return true;
-          }
-        }
-      ]
-    }
-  ]);
-
-  return <RouterProvider router={router} />;
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
