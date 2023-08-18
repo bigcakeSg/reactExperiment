70f8c810359a392a8a1dda9c381c856e97a14b10
bigcakeSg <sebastiengirard@yahoo.fr>
2023-08-18 11:33:58 +0200

    font + styled components

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,7 +1,19 @@
+import styled from 'styled-components';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const Title = styled.h1`
+  font-family: 'Roboto';
+  font-weight: 700;
+  color: red;
+`;
+
 const Home = (userId, setUserId) => {
   return (
     <>
-      <h1>Home Page</h1>
+      <Title>Home Page</Title>
     </>
   );
 };
