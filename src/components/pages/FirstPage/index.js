9ade63ff06854a9fef2c4ab44ef0496984769159
Sébastien Girard <95628952+bigcakeSg@users.noreply.github.com>
2023-08-18 12:21:01 +0200

    Material UI (#7)
    * init material
    
    * font + styled components
    
    * styles
    
    * install material icons
    
    * button icon

--- src/components/pages/FirstPage/index.js
+++ src/components/pages/FirstPage/index.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useFirstPage } from './hooks';
+import { Button } from '@mui/material';
+import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
 
 const FirstPage = () => {
   const { name, loading, userId } = useFirstPage();
@@ -7,8 +9,12 @@ const FirstPage = () => {
   return (
     <>
       <h1>First Page ({userId})</h1>
-      <div>Bonjour {loading ? '...' : name}</div>
-      <Link to={`/`}>Back</Link>
+      <p>Bonjour {loading ? '...' : name}</p>
+      <Link to={`/`}>
+        <Button variant="contained" startIcon={<SettingsBackupRestoreIcon />}>
+          Back
+        </Button>
+      </Link>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-// TODO:
-// StyledComponents
-// Material UI
-// Tests
-// Typescript
-// Param Axios
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import './styles/global-styles.css';
 
